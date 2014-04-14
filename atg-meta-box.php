6256d1bf1a770d31fe6eac87d1743fa4aec35a6f
Alison Barrett <alison@barre.tt>
2014-04-14 11:03:58 -0500

    Update version number

--- atg-meta-box.php
+++ atg-meta-box.php
@@ -3,7 +3,7 @@
  * Plugin Name: ATG Meta Box
  * Plugin URI:
  * Description: A framework for easily adding meta boxes to plugins and themes. Field types include basic form elements as well as color picker, date picker, file upload, and WYSIWYG. Uses jQuery Select2 to make dropdowns searchable and multiple-select elements more usable.
- * Version:     0.1-alpha
+ * Version:     0.2
  * Author:      Alison Barrett
  * Author URI:  http://alisothegeek.com/
  * Text Domain:
